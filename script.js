65b56d39dd12557ae5bdb232cd8677bf3388b77d
jrme421 <jeromepchallet@gmail.com>
2024-01-31 23:46:31 +0900

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function calcTip(billValue) {
-  let tip = 0;
-  let total = 0;
-  if (billValue >= 50 || billValue <= 300) {
-    tip = (billValue * 20) / 100;
-    total = billValue + tip;
-    tips.push[tip];
-    totals.push[total];
-  } else {
-    tip = (billValue * 15) / 100;
-    total = billValue + tip;
-    tips.push[tip];
-    totals.push[total];
-  }
-}
-
-const bills = [125, 555, 44];
 let tips = [];
 let totals = [];
 
-for (i = 0; i <= bills.length; i++) {
-  console.log(calcTip(bills[i]));
+function calcTip(billValue) {
+  let tip = 0;
+  let total = 0;
+  if (billValue >= 50 && billValue <= 300) {
+    tip = (billValue * 15) / 100;
+    total = billValue + tip;
+  } else {
+    tip = (billValue * 20) / 100;
+    total = billValue + tip;
+  }
+  tips.push(tip);
+  totals.push(total);
+}
+
+const bills = [125, 555, 44];
+
+for (let i = 0; i < bills.length; i++) {
+  calcTip(bills[i]);
+  console.log(
+    `The Bill is: ${bills[i]}, Tip is ${tips[i]} and Total is ${totals[i]}`
+  );
 }
